3baf61d83ae020a710117bb845ff462ef03b8d42
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-10-14 12:26:58 +0300

    Сделал инит и деинит для вектора

--- src/main.c
+++ src/main.c
@@ -10,15 +10,39 @@
 	операции определения нижней и верхней границ индекса;
 	размер вектора;
 	инициализация вектора и его очистка;
-	слияние векторов
+	слияние векторов;
+	//
 	случайное заполнение структуры данных;
 	заполнение из файла;
 	сохранение структуры в файл;
 	необходимо уметь обосновать выбранные размеры переменных
 		и временных буферов.
 */
+#define DATAYPE int
+
+typedef struct {
+	size_t max_size;//Максимальная вместимость набора
+	size_t cur_size;//Текущий размер набора
+	DATAYPE *data;
+} SortedVec;
+
+SortedVec* SortedVecInit(size_t const max_size) {
+	SortedVec* ptr = (SortedVec *)malloc(sizeof(SortedVec) + sizeof(DATAYPE) * max_size);
+	if (!ptr)
+		return NULL;
+	ptr->max_size = max_size;
+	ptr->cur_size = 0;
+	ptr->data = (DATAYPE *)(ptr + 1);
+	return ptr;
+}
+
+void SortedVecDeInit(SortedVec **ptr){
+	free(*ptr);
+	*ptr = NULL;
+}
 
 int main(void) {
-	puts("Hello World!");
+	SortedVec* vec = SortedVecInit(100);
+	SortedVecDeInit(&vec);
 	return EXIT_SUCCESS;
 }
