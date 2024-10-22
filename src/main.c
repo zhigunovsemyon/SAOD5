3e19635ebc78f21aaaa6f3438fcde46d5b10e612
Semyon <zhigunovsemen@gmail.com>
2024-10-22 18:18:13 +0300

    Динамический массив. Функции его наполнения. Чтение

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "sortvec.h" //includes stdlib.h
-#include <stdio.h>
+#include <time.h> // time()
+#include <stdio.h> // printf()
 /*Задание: сделать упорядоченный набор элементов на массиве
 Перечень операций:
 	получение элемента по индексу;
@@ -19,10 +20,39 @@
 		и временных буферов.
 */
 
+void ConstFill(DATATYPE *arr, size_t size, DATATYPE val){
+	while (size--)
+		arr[size] = val;//(DATATYPE)size;//rand();
+}
+
+void LinearFill(DATATYPE *arr, size_t size){
+	while (size--)
+		arr[size] = (DATATYPE)size;//rand();
+}
+
+void RandomiseArray(DATATYPE *arr, size_t size){
+	while (size--)
+		arr[size] = rand();
+}
+
 int main(void) {
+	srand((unsigned int)time(NULL));
 	SortedVec *vec = SortedVecInit();
 	if (vec == NULL)
 		return ERR_MALLOC;
+
+	const size_t arrLen = 100;
+	DATATYPE *arr = (DATATYPE *)calloc(sizeof(DATATYPE), arrLen);
+	// RandomiseArray(arr, arrLen);
+	// LinearFill(arr, arrLen);
+	// ConstFill(arr, arrLen,1);
+	SortedVecInsertArray(vec, arrLen, arr);
+
+	free(arr);
+
+	for (size_t i = 0; i < arrLen; i++)
+		printf("[%03li] = %i\n", i, SortedVecGet(vec, (long)i));
+
 	SortedVecDeInit(&vec);
 	return ERR_NO;
 }
